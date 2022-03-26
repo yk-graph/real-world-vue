9f857ac9479e0c066fad797175f903b981fa3970
TatsuyaYokokura <yokokura.tatsuya@gmail.com>
2022-03-26 23:04:02 +0900

    add: checkbox

---
+++ src/components/BaseCheckbox.vue
@@ -0,0 +1,26 @@
+<template>
+  <input
+    v-bind="$attrs"
+    type="checkbox"
+    :checked="modelValue"
+    @change="$emit('update:modelValue', $event.target.checked)"
+    :id="`checkbox-${label}`"
+    class="field"
+  />
+  <label v-if="label" :for="`checkbox-${label}`">{{ label }}</label>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    modelValue: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
--- src/views/EventForm.vue
+++ src/views/EventForm.vue
@@ -35,13 +35,11 @@
 
       <h3>Extras</h3>
       <div>
-        <input type="checkbox" v-model="event.extras.catering" class="field" />
-        <label>Catering</label>
+        <BaseCheckbox v-model="event.extras.catering" label="Catering" />
       </div>
 
       <div>
-        <input type="checkbox" v-model="event.extras.music" class="field" />
-        <label>Live music</label>
+        <BaseCheckbox v-model="event.extras.music" label="Live music" />
       </div>
 
       <button type="submit">Submit</button>
@@ -52,11 +50,13 @@
 <script>
 import BaseInput from '@/components/BaseInput.vue'
 import BaseSelect from '@/components/BaseSelect.vue'
+import BaseCheckbox from '@/components/BaseCheckbox.vue'
 
 export default {
   components: {
     BaseInput,
-    BaseSelect
+    BaseSelect,
+    BaseCheckbox
   },
 
   data() {
